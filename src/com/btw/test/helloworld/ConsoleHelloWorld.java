dc088a1716dce2fec5ba84965d74b12322ce2dcc
Alexander Borisov <alexb.btw@gmail.com>
2010-09-18 22:13:21 +0300

    First commit

---
+++ src/com/btw/test/helloworld/ConsoleHelloWorld.java
@@ -0,0 +1,44 @@
+package com.btw.test.helloworld;
+
+/**
+ * Simple console 'Hello World' class
+ * 
+ * @author Alexander Borisov
+ *
+ */
+public class ConsoleHelloWorld {
+
+	/*
+	 * System properties names
+	 */
+	private static final String OS_NAME_PROPERTY = "os.name";
+	private static final String USER_NAME_PROPERTY = "user.name";	
+
+	/*
+	 * Default values
+	 */			
+	private static final String DEFAULT_OS_NAME = "<Unknown>";
+	private static final String DEFAULT_USER_NAME = "Incognito";	
+	
+	/**
+	 * Constructor
+	 */
+	ConsoleHelloWorld(){
+		writeInfo(System.getProperty(USER_NAME_PROPERTY, DEFAULT_USER_NAME), 
+			System.getProperty(OS_NAME_PROPERTY, DEFAULT_OS_NAME));
+	}
+	
+	protected void writeInfo(String userName, String osName){
+		System.out.printf("Hello, %s!%nYou're using '%s' operation system, aren't you?%n", userName, osName);
+	}
+	
+	/**
+	 * Entrance point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		new ConsoleHelloWorld();
+	}
+
+}
