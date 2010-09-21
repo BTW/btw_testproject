b6e58f13c3945463f95531bb158cda61e7fcdc51
Vitaliy Gerya <VitaliyG.btw@gmail.com>
2010-09-21 21:02:43 +0300

    Insert Hello World class.

--- src/com/btw/test/helloworld/ConsoleHelloWorld.java
+++ src/com/btw/test/helloworld/ConsoleHelloWorld.java
@@ -2,7 +2,7 @@ package com.btw.test.helloworld;
 
 /**
  * Simple console 'Hello World' class
- * 
+ *
  * @author Alexander Borisov
  *
  */
@@ -12,32 +12,33 @@ public class ConsoleHelloWorld {
 	 * System properties names
 	 */
 	private static final String OS_NAME_PROPERTY = "os.name";
-	private static final String USER_NAME_PROPERTY = "user.name";	
+	private static final String USER_NAME_PROPERTY = "user.name";
 
 	/*
 	 * Default values
-	 */			
+	 */
 	private static final String DEFAULT_OS_NAME = "<Unknown>";
-	private static final String DEFAULT_USER_NAME = "Incognito";	
-	
+	private static final String DEFAULT_USER_NAME = "Incognito";
+
 	/**
 	 * Constructor
 	 */
 	ConsoleHelloWorld(){
-		writeInfo(System.getProperty(USER_NAME_PROPERTY, DEFAULT_USER_NAME), 
+		writeInfo(System.getProperty(USER_NAME_PROPERTY, DEFAULT_USER_NAME),
 			System.getProperty(OS_NAME_PROPERTY, DEFAULT_OS_NAME));
 	}
-	
+
 	protected void writeInfo(String userName, String osName){
 		System.out.printf("Hello, %s!%nYou're using '%s' operation system, aren't you?%n", userName, osName);
+		System.out.println("I can't see Hello World words, where are they?");
 	}
-	
+
 	/**
 	 * Entrance point
-	 * 
+	 *
 	 * @param args
 	 */
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		new ConsoleHelloWorld();
 	}
 
---
+++ src/com/btw/test/helloworld/ReallyHelloWorld.java
@@ -0,0 +1,12 @@
+package com.btw.test.helloworld;
+
+public class ReallyHelloWorld {
+	public ReallyHelloWorld() {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		new ReallyHelloWorld();
+	}
+
+}
