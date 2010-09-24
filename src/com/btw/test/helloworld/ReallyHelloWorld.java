a8152f126516a90054056f253a0ca4471d982d4c
alexb <alexb-btw@gmail.com>
2010-09-25 01:32:52 +0300

    Comment added

--- src/com/btw/test/helloworld/ReallyHelloWorld.java
+++ src/com/btw/test/helloworld/ReallyHelloWorld.java
@@ -1,8 +1,12 @@
 package com.btw.test.helloworld;
 
+/**
+ * Probably, the simplest implementation of the 'Hello World' in Java
+ *
+ */
 public class ReallyHelloWorld {
 	public ReallyHelloWorld() {
-		System.out.println("Hello World!");
+		System.out.println("Hello, World!\nYou are weird :)");
 	}
 
 	public static void main(String[] args) {
